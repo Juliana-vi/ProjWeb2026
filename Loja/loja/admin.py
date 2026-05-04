69d581f7ac7b0de3bb861d6d1b435820c2adf96e
Juliana-vi <j.vianna@escolar.ifrn.edu.br>
2026-05-04 09:47:39 -0300

    últimas alterações do cap 5 e atualizando o venv e db compactado

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -17,12 +17,13 @@ admin.site.register(Fabricante, FabricanteAdmin) #adiciona a interface do adm
 admin.site.register(Categoria)
 admin.site.register(Produto, ProdutoAdmin)
 
-# código pra acessar: http://127.0.0.1:8080/admin
+# link pra acessar: http://127.0.0.1:8080/admin
 # código pra rodar: python manage.py runserver 127.0.0.1:8080
 # ORDEM PARA RODAR O VENV: 
 # cd loja
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-# python -m venv venv
+# winget install Python.Python.3.11
+# py -3.11 -m venv venv
 # venv\Scripts\activate
 # pip install django Django==4.2.7
 # pip install pillow
