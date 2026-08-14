d0865c9f4d909dc1b5a75ce62c1cab450fe75084
Juliana-vi <j.vianna@escolar.ifrn.edu.br>
2026-08-14 10:26:23 -0300

    aula 14/08 - cap 10 - concluído msm - melhorado

--- Loja/loja/urls/AuthUrls.py
+++ Loja/loja/urls/AuthUrls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from loja.views.AuthView import login_view
+from loja.views.AuthView import login_view, register_view
 
 urlpatterns = [
     path("login", login_view, name='login'),
